d630b0cf350b2781f3aefa37371f298d75602200
AntiD2ta <mtpotro41@gmail.com>
2025-12-01 10:32:40 -0300

    Remove unnecessary package comment

--- loggers/gin.go
+++ loggers/gin.go
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package loggers provides logging utilities for the block relay.
 package loggers
 
 import (
